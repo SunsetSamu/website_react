41dd80d5462009f07a90170fbb721eefe8aa3c1c
SunsetSamu <vsamuelalfredo@gmail.com>
2025-01-03 19:50:25 -0400

    fix: update homepage URL and deployment directory in package.json and configure base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/deploy-test/', // Reemplaza con el nombre de tu repositorio
   plugins: [react()],
 })
